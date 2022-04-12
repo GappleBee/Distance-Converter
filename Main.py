abb4e40b698a0e0f3844f3adc33fd7a6b2b51000
GappleBee <85840539+GappleBee@users.noreply.github.com>
2022-04-12 21:52:12 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-def Kilometres():  # Converting to kilometres
+def Kilometres():  # Function for converting to kilometres
     global miles
     global kilometres
     miles = entryBox.get()
@@ -6,15 +6,17 @@ def Kilometres():  # Converting to kilometres
     outputBox["text"] = str(miles) + " mile(s) is " + str(kilometres) + " kilometre(s)"
 
 
-def Miles():  # Converting to miles
+def Miles():  # Function for converting to miles
     global miles2
     global kilometres2
     kilometres2 = entryBox2.get()
     miles2 = float(kilometres2) * 0.6214
-    outputBox2["text"] = str(kilometres2) + " kilometre(s) is " + str(miles2) + " mile(s)"
+    outputBox2["text"] = (
+        str(kilometres2) + " kilometre(s) is " + str(miles2) + " mile(s)"
+    )
 
 
-# Setting the original values for the distances
+# Setting the original values for the distances so they can be used in the GUI as soon as it starts up
 miles = 0
 kilometres = 0
 miles2 = 0
@@ -25,7 +27,7 @@ window = Tk()
 window.title("Distance Converter")
 window.geometry("600x400")
 
-# Setting up the messages, boxes and buttons that will be used to convert to kilometres
+# Creating the messages, boxes and buttons required for converting to kilometres
 label = Label(window, text="Convert from miles to kilometres:")
 label.pack()
 
@@ -35,10 +37,12 @@ entryBox.pack()
 button = Button(window, text="Convert to kilometres", command=Kilometres)
 button.pack()
 
-outputBox = Message(window, text=str(miles) + " mile(s) is " + str(kilometres) + " kilometre(s)")
+outputBox = Message(
+    window, text=str(miles) + " mile(s) is " + str(kilometres) + " kilometre(s)"
+)
 outputBox.pack()
 
-# Setting up the messages, boxes and buttons that will be used to convert to miles
+# Creating the messages, boxes and buttons required for converting to miles
 label2 = Label(window, text="Convert from kilometres to miles:")
 label2.pack()
 
@@ -48,7 +52,9 @@ entryBox2.pack()
 button2 = Button(window, text="Convert to miles", command=Miles)
 button2.pack()
 
-outputBox2 = Message(window, text=str(kilometres2) + " kilometre(s) is " + str(miles2) + " mile(s)")
+outputBox2 = Message(
+    window, text=str(kilometres2) + " kilometre(s) is " + str(miles2) + " mile(s)"
+)
 outputBox2.pack()
 
 window.mainloop()
