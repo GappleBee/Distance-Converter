fd35a77c08a2a4e8cf469f65587673d865a886fc
GappleBee <85840539+GappleBee@users.noreply.github.com>
2022-04-13 20:14:34 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
-def Kilometres():
+def Kilometres():  # Function for converting into kilometres
     miles = entryBox.get()
     kilometres = float(miles) * 1.6093
     outputBox["text"] = str(miles) + " mile(s) is " + str(kilometres) + " kilometre(s)"
 
 
-def Miles():
+def Miles():  # Function for converting into miles
     kilometres2 = entryBox2.get()
     miles2 = float(kilometres2) * 0.6214
     outputBox2["text"] = (
@@ -12,19 +12,22 @@ def Miles():
     )
 
 
+# Setting up the original values for the distances so that they can be displayed before anything is typed in or clicked
 miles = 0
 kilometres = 0
 miles2 = 0
 kilometres2 = 0
 
+# Setting up the window
 window = Tk()
 window.title("Distance Converter")
 window.geometry("600x400")
 
+# Setting up the messages, buttons and boxes for converting into kilometres
 label = Label(window, text="Convert from miles to kilometres:")
 label.pack()
 
-entryBox = Entry(window, text=0)
+entryBox = Entry(window, text="")
 entryBox.pack()
 
 button = Button(window, text="Convert to kilometres", command=Kilometres)
@@ -35,10 +38,11 @@ outputBox = Message(
 )
 outputBox.pack()
 
+# Setting up the messages, buttons and boxes for converting into miles
 label2 = Label(window, text="Convert from kilometres to miles:")
 label2.pack()
 
-entryBox2 = Entry(window, text=0)
+entryBox2 = Entry(window, text="")
 entryBox2.pack()
 
 button2 = Button(window, text="Convert to miles", command=Miles)
